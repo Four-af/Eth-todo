112b46392482755217d870a0fc3031c04157179c
Four <aimzfatima08@gmail.com>
2022-09-11 19:48:37 +0530

    created the event

--- test/TodoList.test.js
+++ test/TodoList.test.js
@@ -32,5 +32,17 @@ contract("TodoList", (accounts) => {
 
   })
 
+  it('create tasks', async()=>{
+    const result = await this.todoList.createTask('Four says a new task')
+    const taskCount = await this.todoList.taskCount()
+    assert.equal(taskCount,2)
+    console.log(result)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(),2)
+    assert.equal(event.content, 'Four says a new task')
+    assert.equal(event.completed, false)
+    
+  })
+
 
 });
